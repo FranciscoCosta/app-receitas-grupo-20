c0b9f65084a9c7e4bfc4f60aa98d94291af724d9
Lucas Dias <lucaalencarID@gmail.com>
2022-10-25 19:26:42 -0300

    refatorando para criar o componente recipies

--- src/Pages/Drinks/Drinks.jsx
+++ src/Pages/Drinks/Drinks.jsx
@@ -1,46 +1,15 @@
-import React, { useEffect, useContext, useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import { Context } from '../../Context/Context';
+
 import Header from '../../Components/Header';
 import Footer from '../../Components/Footer';
+import Recipes from '../../Components/Recipes';
 
 function Drinks({ history }) {
-  const LIMIT_ELEMENTS = 12;
-  const [bebida, setbebida] = useState([]);
-  const { dataApi } = useContext(Context);
-  useEffect(() => {
-    const { drinks } = dataApi;
-    const value = (drinks === null || drinks === undefined) ? [] : drinks;
-    if (value.length === 1) {
-      history.push(`/drinks/${value[0].idDrink}`);
-    } else {
-      const newValue = value.slice(0, LIMIT_ELEMENTS);
-      setbebida(newValue);
-    }
-  }, [dataApi]);
   return (
     <div className="Drinks">
       <Header title="Drinks" perfilBool searchBool type="drinks" />
-      {bebida.map((drink, index) => (
-        <div
-          className="Drinks__card-info"
-          data-testid={ `${index}-recipe-card` }
-          key={ drink.idDrink }
-        >
-          <img
-            className="Drinks__card-img"
-            src={ drink.strDrinkThumb }
-            alt={ drink.strDrink }
-            data-testid={ `${index}-card-img` }
-          />
-          <p
-            className="Drinks__card-tags"
-            data-testid={ `${index}-card-name` }
-          >
-            {drink.strDrink}
-          </p>
-        </div>
-      ))}
+      <Recipes page="drinks" history={ history } />
       <Footer />
     </div>
   );
--- src/Pages/Meals/Meals.jsx
+++ src/Pages/Meals/Meals.jsx
@@ -1,56 +1,24 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
+
 import Header from '../../Components/Header';
 import Footer from '../../Components/Footer';
-import { Context } from '../../Context/Context';
+import Recipes from '../../Components/Recipes';
 
 function Meals({ history }) {
-  const LIMIT_ELEMENTS = 11;
-  const { dataApi } = useContext(Context);
-  const [comida, setcomida] = useState([]);
-  useEffect(() => {
-    const { meals } = dataApi;
-    const value = (meals === null || meals === undefined) ? [] : meals;
-    if (value.length === 1) {
-      history.push(`/meals/${value[0].idMeal}`);
-    } else {
-      const newValue = value.slice(0, LIMIT_ELEMENTS);
-      setcomida(newValue);
-    }
-  }, [dataApi]);
-
   return (
     <div className="Meals">
       <Header title="Meals" perfilBool searchBool type="meals" />
-      {comida.map((food, index) => (
-        <div
-          className="Meals__card-info"
-          data-testid={ `${index}-recipe-card` }
-          key={ food.idMeal }
-        >
-          <img
-            className="Meals__card-img"
-            src={ food.strMealThumb }
-            alt={ food.strMeal }
-            data-testid={ `${index}-card-img` }
-          />
-          <p
-            className="Meals__card-tags"
-            data-testid={ `${index}-card-name` }
-          >
-            {food.strMeal}
-          </p>
-        </div>
-      ))}
+      <Recipes page="meals" history={ history } />
       <Footer />
     </div>
   );
 }
-
-export default Meals;
 
 Meals.propTypes = {
   history: PropTypes.shape({
     push: PropTypes.func,
   }),
 }.isRequired;
+
+export default Meals;
